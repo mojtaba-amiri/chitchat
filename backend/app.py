f80ff7c96ec14d62c13f57ad3e910ae732a3774b
mojtaba-amiri <mojtaba.amiri@alayacare.com>
2023-10-13 22:44:32 -0400

    refactor endpoints small improvements

--- backend/app.py
+++ backend/app.py
@@ -15,40 +15,47 @@ app.config['UPLOAD_FOLDER'] = '/files'
 
 jwt = JWTManager(app) # initialize JWTManager
 app.config['JWT_SECRET_KEY'] = config.JWT.get('secret')
+
+answer_prompt = config.GPT.get('answer_prompt')
+sum_prompt_before = config.GPT.get('sum_prompt_before')
+sum_prompt_after = config.GPT.get('sum_prompt_after')
+token_expire_hours = float(config.JWT.get('token_expire_hours'))
 # app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=30) # define the life span of the token
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=2)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=token_expire_hours)
 os.makedirs(os.path.join(app.instance_path, 'files'), exist_ok=True)
 
 @app.route("/api/v1/register", methods=["POST"])
 async def register():
-    req = request.get_json() # store the json body request
-    print(f"request --- {req}")
-    user_id = req["user_id"]
-    platform = req["platform"]
-    url = f"https://api.qonversion.io/v3/users/{user_id}/entitlements"
-    qonversion_key = config.QONVERSION.get('api_key')
-    headers = {
-        "accept": "application/json",
-        "Content-Type": "application/json",
-        "Platform": f"{platform}",
-        "Authorization": f"Bearer {qonversion_key}" # Qonversion KEY test_PV77YHL7qnGvsdmpTs7gimsxUvY-Znl2
-    }
+    try:
+        req = request.get_json() # store the json body request
+        print(f"request --- {req}")
+        user_id = req["user_id"]
+        platform = req["platform"]
+        url = f"https://api.qonversion.io/v3/users/{user_id}/entitlements"
+        qonversion_key = config.QONVERSION.get('api_key')
+        headers = {
+            "accept": "application/json",
+            "Content-Type": "application/json",
+            "Platform": f"{platform}",
+            "Authorization": f"Bearer {qonversion_key}" # Qonversion KEY test_PV77YHL7qnGvsdmpTs7gimsxUvY-Znl2
+        }
 
-    response = requests.get(url, headers=headers)
-    if (response.status_code == 200) :
-        result = response.json()
-        if (len(result["data"])>0): 
-            product_id = result["data"][0]["product"]["product_id"]
-            if (product_id == None): 
-                return jsonify(code="403", err="Registration not valid."), 403
-            else:
-                # make the jwt 
-                # Add user id 
-                access_token = create_access_token(identity=user_id)
-                return jsonify(access_token=access_token), 200
-    else: 
-         return jsonify(code="501", err="Couldn't register!"), 501
-
+        response = requests.get(url, headers=headers)
+        if (response.status_code == 200) :
+            result = response.json()
+            if (len(result["data"])>0): 
+                product_id = result["data"][0]["product"]["product_id"]
+                if (product_id == None): 
+                    return jsonify(code="403", err="Registration not valid."), 403
+                else:
+                    # make the jwt 
+                    # Add user id 
+                    access_token = create_access_token(identity=user_id)
+                    return jsonify(access_token=access_token), 200
+        else: 
+            return jsonify(code="401", err="Couldn't register!"), 501
+    except:
+        return jsonify(code="501", err="Couldn't register. Server Error!"), 501
 
 @app.route("/api/v1/transcribe", methods=["POST"])
 @jwt_required()
@@ -69,26 +76,29 @@ async def transcribe():
 async def answer():
     # user_id = get_jwt_identity() # Get the identity of the current user
     # file = request.files['file'] # the audio file 
-    text = request.get_json()["text"]
-    r = await openai.ChatCompletion.acreate(model="gpt-3.5-turbo", 
-                                            messages=[
-                                                {"role": "user", "content": f"You are an expert in this field. Answer this question in less than 250 words in the same tone: {text}"}
-                                                ])
-    # get teh file from request 
-    return jsonify(response = r), 200
+    try:
+        text = request.get_json()["text"]
+        r = await openai.ChatCompletion.acreate(model="gpt-3.5-turbo", 
+                                                messages=[{"role": "user", "content": f"{answer_prompt} {text}"}])
+        # get teh file from request 
+        return jsonify(response = r), 200
+    except:
+        return jsonify(error="Something Went wrong!"), 500
 
 
 @app.route("/api/v1/summarize", methods=["POST"])
 @jwt_required()
 async def summarize():
     # user_id = get_jwt_identity() # Get the identity of the current user
-    text = request.get_json()["text"]
-    r = await openai.ChatCompletion.acreate(model="gpt-3.5-turbo", 
-                                            messages=[
-                                                {"role": "user", "content": f"Summarize this to few bullet points: {text} \m each bullet point one sentence."}
-                                                ])
-    # get teh file from request 
-    return jsonify(response = r), 200
+    try:
+        text = request.get_json()["text"]
+        r = await openai.ChatCompletion.acreate(model="gpt-3.5-turbo", 
+                                                messages=[{"role": "user", "content": f"{sum_prompt_before}{text}{sum_prompt_after}"}])
+        # get teh file from request 
+        return jsonify(response = r), 200
+    except:
+        return jsonify(error="Something Went wrong!"), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
